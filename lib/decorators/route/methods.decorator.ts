6f7187bfd1c1bf5608c2469fc31d5d7414a8989c
sajjadmrx <sajjadzibaee102@gmail.com>
2022-10-13 19:35:08 +0330

    add methods

--- lib/decorators/route/methods.decorator.ts
+++ lib/decorators/route/methods.decorator.ts
@@ -9,3 +9,23 @@ export function Get(path: string, options?: RouteOptions) {
 export function Post(path: string, options?: RouteOptions) {
     return RouteHandler(ApiMethods.POST, path, options)
 }
+
+export function Put(path: string, options?: RouteOptions) {
+    return RouteHandler(ApiMethods.PUT, path, options)
+}
+
+export function Patch(path: string, options?: RouteOptions) {
+    return RouteHandler(ApiMethods.PATCH, path, options)
+}
+
+export function Delete(path: string, options?: RouteOptions) {
+    return RouteHandler(ApiMethods.DELETE, path, options)
+}
+
+export function Head(path: string, options?: RouteOptions) {
+    return RouteHandler(ApiMethods.HEAD, path, options)
+}
+
+export function Options(path: string, options?: RouteOptions) {
+    return RouteHandler(ApiMethods.OPTIONS, path, options)
+}
